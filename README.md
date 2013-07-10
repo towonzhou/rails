d75826af989c48857396b662f01fcd537ea39c72
zhouhuan <zhouhuan@redflag-linux.com>
2013-07-10 11:19:48 +0800

    rails new upload

---
+++ README.md
@@ -0,0 +1,10 @@
+rails-testf
+===========
+
+for rails test
+面试官说会做一个上传的功能是从新手到初级的必然经历阶段,所以试试  
+步骤和问题  
+1. rails new upload (bundle install很慢,gem列表已经在本机存在,应该有一个直接用的方法,有待研究) 
+   (解决方法,因为本机已经全部安装好了基本的Gem,所以运行gem server,然后把Gemfile的source指向"http://127.0.0.1"这样很快)  
+   (据说http://ruby.taobao.org很快,没试过)  
+2.  
---
+++ upload/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upload::Application.config.secret_token = '171d2b52e6fd37fb3ec6acdfa324622a29ed3c80eb04aabcdfe8bb134a677c5b7a616135b96223bcc4465326986e851833a677ddeee71ed93c33cf6eec923203'
